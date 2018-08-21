0df43f4a144e932f69f7e27d683d7f57129961c0
Noah Gibson <135noahg@gmail.com>
2018-08-21 14:30:59 -0400

    added entropy and information gain calculations to Math

--- java-core/SILO/src/main/java/silo/math/Math.java
+++ java-core/SILO/src/main/java/silo/math/Math.java
@@ -1,10 +1,8 @@
 package silo.math;
 
-import silo.data.DataRow;
+import silo.data.DataFrame;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Class implementing basic mathematical functions.
@@ -73,6 +71,93 @@ public class Math {
             }
         }
         return winner;
+    }
+
+    /**
+     * Returns the logarithm of the given value using base 2.
+     *
+     * @param x
+     *      the value to compute the logarithm of
+     *
+     * @return the log base 2 of the given value
+     */
+    public static double log2(double x) {
+        return java.lang.Math.log(x) / java.lang.Math.log(2);
+    }
+
+    /**
+     * Computes the entropy of the given list of discrete values.
+     *
+     * @param values
+     *      the values to compute the entropy of
+     *
+     * @return the entropy of the given set of values
+     */
+    public static <T> double entropy(List<T> values) {
+        Map<T, Integer> classCounts = new HashMap<>();
+        for (T value : values) {
+            if (classCounts.containsKey(value)) {
+                classCounts.put(value, classCounts.get(value) + 1);
+            } else {
+                classCounts.put(value, 1);
+            }
+        }
+        double entropy = 0.0;
+        for (Map.Entry<T, Integer> valueCount : classCounts.entrySet()) {
+            double prob = (double) valueCount.getValue() / values.size();
+            entropy += prob * log2(prob);
+        }
+        return -entropy;
+    }
+
+    /**
+     * Calculates the information gain of the given labels if splitting on the given feature.
+     *
+     * @param feature
+     *      the feature of the data set to split on
+     * @param labels
+     *      the labels of the data set
+     *
+     *
+     * @return the information gain
+     *
+     * @throws IllegalArgumentException if either feature or labels is {@code null}, or if feature and labels do not
+     *      have the same size
+     */
+    public static <F, L> double informationGain(List<F> feature, List<L> labels) throws IllegalArgumentException {
+        if (feature == null) {
+            throw new IllegalArgumentException("feature must not be null");
+        } else if (labels == null) {
+            throw new IllegalArgumentException("labels must not be null");
+        }
+        if (feature.size() != labels.size()) {
+            throw new IllegalArgumentException("feature and labels must be of same size");
+        }
+
+        /*
+          * Map containing each possible value of the attribute column as the keys, and the list of all labels of that
+          * attribute value as the values
+          */
+        Map<F, List<L>> valueLabels = new HashMap<>();
+
+        // Initializing the valueLabels map
+        for (int i = 0; i < feature.size(); i++) {
+            F attrVal = feature.get(i);
+            if (valueLabels.containsKey(attrVal)) {
+                valueLabels.get(attrVal).add(labels.get(i));
+            } else {
+                List<L> attrLabels = new ArrayList<>();
+                attrLabels.add(labels.get(i));
+                valueLabels.put(attrVal, attrLabels);
+            }
+        }
+
+        // Calculating the information gain
+        double ig = entropy(labels);
+        for (Map.Entry<F, List<L>> valueLabel : valueLabels.entrySet()) {
+            ig -= ((double) valueLabel.getValue().size() / feature.size()) * entropy(valueLabel.getValue());
+        }
+        return ig;
     }
 
 }
